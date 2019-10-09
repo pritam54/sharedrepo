3183d17b885077b479b38bbf3d3dd358eacd3c93
nikhil <nikhilsanghi4@gmail.com>
2019-10-09 16:16:59 +0530

    Jee le zara

---
+++ java_project/src/java_project/Bus.java
@@ -0,0 +1,9 @@
+package java_project;
+
+public class Bus {
+	
+	public String name;
+	
+	public String id;
+
+}
---
+++ java_project/src/java_project/panja.java
@@ -0,0 +1,8 @@
+package java_project;
+
+public class panja {
+
+	public String chutiya;
+
+	public String bohot_bada;
+}
